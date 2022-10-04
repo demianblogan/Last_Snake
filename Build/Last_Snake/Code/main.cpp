d718c60b9454d3e3d6356d3695cd616ff78defae
Demian Blogan <demianblogan@gmail.com>
2022-10-04 23:38:44 +0300

    Comment is corrected.

--- Build/Last_Snake/Code/main.cpp
+++ Build/Last_Snake/Code/main.cpp
@@ -1,8 +1,8 @@
 #include <Windows.h>
 
-// The main function looks exactly how is has to look for not showing any warning from Microsoft compiler
-// Windows applications require "WinMain" or "wWinMain" function for entry point. I chose the second function because
-// it handles Unicode characters and my application uses them to, so it's just for convention.
+// The main function looks exactly how is has to look for not showing any warnings from Microsoft compiler.
+// Windows applications require "WinMain" or "wWinMain" function for entry point. I chose the second function, because
+// it handles Unicode characters and my application uses them too, so it's just for the code convention.
 int wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE previousInstance, _In_ LPWSTR commandLine, _In_ int commandShow)
 {
 	return 0;
