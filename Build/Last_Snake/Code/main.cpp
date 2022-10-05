9a611a9fbb04b5fca6c53fa0257ac2fe87378955
demianblogan <demianblogan@gmail.com>
2022-10-05 21:06:17 +0300

    SFML library is added and linked to the projbect.

--- Build/Last_Snake/Code/main.cpp
+++ Build/Last_Snake/Code/main.cpp
@@ -1,9 +1,41 @@
+#include <SFML/Graphics.hpp>
 #include <Windows.h>
+
+// Link all SFML libraries:
+#if defined(_DEBUG) || defined(DEBUG)
+#pragma comment(lib, "sfml-system-d.lib")
+#pragma comment(lib, "sfml-window-d.lib")
+#pragma comment(lib, "sfml-graphics-d.lib")
+#pragma comment(lib, "sfml-audio-d.lib")
+#else
+#pragma comment(lib, "sfml-system.lib")
+#pragma comment(lib, "sfml-window.lib")
+#pragma comment(lib, "sfml-graphics.lib")
+#pragma comment(lib, "sfml-audio.lib")
+#endif
 
 // The main function looks exactly how is has to look for not showing any warnings from Microsoft compiler.
 // Windows applications require "WinMain" or "wWinMain" function for entry point. I chose the second function, because
 // it handles Unicode characters and my application uses them too, so it's just for the code convention.
 int wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE previousInstance, _In_ LPWSTR commandLine, _In_ int commandShow)
 {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
 	return 0;
 }
